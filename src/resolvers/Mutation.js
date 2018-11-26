4cbf352647be457281152ee90231115901a8ed9e
carlosloureda <balanzeneto@gmail.com>
2018-11-26 03:13:03 +0100

    Server Side GraphQL. closes #4Creates and configures PrismaCreates and configures GraphQL YogaBuilt our first Query and our first Mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,17 @@
-const mutations = {};
+const Mutations = {
+  createItem: async (parent, args, ctx, info) => {
+    // TODO: check if they are logged in
+    console.log("hello");
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+    return item;
+  }
+};
 
-module.exports = mutations;
+module.exports = Mutations;
