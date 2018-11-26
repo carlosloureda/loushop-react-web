44b9a5fe047867f44218f7b4180cfc7951a14542
carlosloureda <balanzeneto@gmail.com>
2018-11-26 17:16:43 +0100

    (#4) React Meets GraphQL

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,9 +1,8 @@
-import React from "react";
-import Link from "next/link";
+import Items from "../components/Items";
 
 const Home = props => (
   <div>
-    <p> Hey! Home</p>
+    <Items />
   </div>
 );
 export default Home;
