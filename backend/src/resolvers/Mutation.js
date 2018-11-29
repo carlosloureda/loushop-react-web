a72b66ec13cca64741814c92366b3f1ed5559c4b
carlosloureda <balanzeneto@gmail.com>
2018-11-29 03:21:05 +0100

    #4. Updating Items with Queries and Mutations

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -10,6 +10,22 @@ const Mutations = {
       info
     );
     return item;
+  },
+  updateItem: async (parent, args, ctx, info) => {
+    // first take a copy of the updates
+    const updates = { ...args };
+    // remove the ID from the updates;
+    delete updates.id;
+    // run the update method
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id
+        }
+      },
+      info
+    );
   }
 };
 
