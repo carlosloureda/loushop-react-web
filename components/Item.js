dc7c2586fab707b26590b22baec704552813b6d0
carlosloureda <balanzeneto@gmail.com>
2018-11-29 03:45:05 +0100

    #4 Delete Items

--- components/Item.js
+++ components/Item.js
@@ -5,7 +5,7 @@ import Title from "./styles/Title";
 import ItemStyles from "./styles/ItemStyles";
 import PriceTag from "./styles/PriceTag";
 import formatMoney from "../lib/formatMoney";
-
+import DeleteItem from "./DeleteItem";
 class Item extends Component {
   static propTypes = {
     item: propTypes.object.isRequired
@@ -38,7 +38,7 @@ class Item extends Component {
             <a>Edit </a>
           </Link>
           <button> Add to cart</button>
-          <button> Delete</button>
+          <DeleteItem id={item.id}>Delete Item</DeleteItem>
         </div>
       </ItemStyles>
     );
