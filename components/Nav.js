4af885d1a9a4241dc55167ea3faadc391a2685f0
carlosloureda <balanzeneto@gmail.com>
2018-11-30 05:19:41 +0100

    #6.4 Sign in Form and Custom Error Handling

--- components/Nav.js
+++ components/Nav.js
@@ -2,32 +2,35 @@ import Link from "next/link";
 import NavStyles from "./styles/NavStyles";
 import User from "./User";
 
+// if (me) return <p>{me.name}</p>; // return null; // )}
 const Nav = () => (
-  <NavStyles>
-    <User>
-      {/* Interesting detructuration */}
-      {({ data: { me }, loading, error }) => {
-        console.log(me);
-        if (me) return <p>{me.name}</p>;
-        return null;
-      }}
-    </User>
-    <Link href="/items">
-      <a>Shop</a>
-    </Link>
-    <Link href="/sell">
-      <a>Sell!</a>
-    </Link>
-    <Link href="/signup">
-      <a>signup</a>
-    </Link>
-    <Link href="/orders">
-      <a>orders</a>
-    </Link>
-    <Link href="/me">
-      <a>account</a>
-    </Link>
-  </NavStyles>
+  <User>
+    {({ data: { me }, loading, error }) => (
+      <NavStyles>
+        <Link href="/items">
+          <a>Shop</a>
+        </Link>
+        {me && (
+          <>
+            <Link href="/sell">
+              <a>Sell</a>
+            </Link>
+            <Link href="/orders">
+              <a>orders</a>
+            </Link>
+            <Link href="/me">
+              <a>account</a>
+            </Link>
+          </>
+        )}
+        {!me && (
+          <Link href="/signup">
+            <a>Sign In</a>
+          </Link>
+        )}
+      </NavStyles>
+    )}
+  </User>
 );
 
 export default Nav;
