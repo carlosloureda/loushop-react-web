88e68b10db01cb0d83db8bf76f1c880f5833ad2e
carlosloureda <balanzeneto@gmail.com>
2018-12-04 04:08:43 +0100

    #6.11 Permissions Management

---
+++ pages/permissions.js
@@ -0,0 +1,11 @@
+import PleaseSignin from "../components/PleaseSignin";
+import Permisisions from "../components/Permissions";
+
+const PermissionsPage = props => (
+  <div>
+    <PleaseSignin>
+      <Permisisions />
+    </PleaseSignin>
+  </div>
+);
+export default PermissionsPage;
