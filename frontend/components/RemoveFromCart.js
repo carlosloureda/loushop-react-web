692c66f842ca824dd7c31743a598a5a29207cfd7
carlosloureda <balanzeneto@gmail.com>
2018-12-18 01:22:43 +0100

    #6.6. (Cart) Optimistic Response && Cache Updates with Apollo

--- frontend/components/RemoveFromCart.js
+++ frontend/components/RemoveFromCart.js
@@ -3,6 +3,7 @@ import { Mutation } from "react-apollo";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 import gql from "graphql-tag";
+import { CURRENT_USER_QUERY } from "./User";
 
 const REMOVE_FROM_CART_MUTATION = gql`
   mutation removeFromCart($id: ID!) {
@@ -26,12 +27,30 @@ class RemoveFromCart extends Component {
   static propTypes = {
     id: PropTypes.string.isRequired
   };
+
+  // This gets called as soon as we get a response back from tghe server after a
+  // mutation has been performed. With the optimisticResponse gets called twice,
+  //   one on client and another time when server responses
+  update = (cache, payload) => {
+    const data = cache.readQuery({ query: CURRENT_USER_QUERY });
+    const cartItemId = payload.data.removeFromCart.id;
+    data.me.cart = data.me.cart.filter(cartItem => cartItem.id !== cartItemId);
+    cache.writeQuery({ query: CURRENT_USER_QUERY, data });
+  };
   render() {
     return (
       <Mutation
         mutation={REMOVE_FROM_CART_MUTATION}
         variables={{ id: this.props.id }}
         refetchQueries={[{ query: CURRENT_USER_QUERY }]}
+        update={this.update}
+        optimisticResponse={{
+          __typename: "Mutation",
+          removeFromCart: {
+            __typename: "CartItem",
+            id: this.props.id
+          }
+        }}
       >
         {(removeFromCart, { loading, error }) => (
           <BigButton
